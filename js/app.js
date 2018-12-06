fc6a2dfb6d4abbe4d378b2c459117c24e9a3f1bd
David Arango <arango.david8@gmail.com>
2018-12-05 22:14:16 -0500

    Rounded down the obstacle mph

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 var Obstacle = function(x, y, mph) {
- //Final Test git contributions 
     this.x = 400;
     this.y = 300;
-    this.mph = Math.random() * 100;
+    this.mph = Math.floor(Math.random() * 100);
     this.cover = 'images/enemy-bug.png';
 };
 
