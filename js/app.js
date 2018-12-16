9b663b7afd48566cae9cf543c5b76304054964c9
David Arango <davidarango@Davids-MBP.attlocal.net>
2018-12-15 21:35:12 -0500

    Added browser-sync to package.json devDependencies, added update function to the obstacle class prototype chain

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 var Obstacle = function(x, y, mph) {
-    this.x = 400;
-    this.y = 300;
+    this.x = Math.floor(Math.random() * 400); //px length of land grid x-axis
+    this.y = Math.floor(Math.random() * 300); //px length of land grid y-axis
     this.mph = Math.floor(Math.random() * 100);
     this.cover = 'images/enemy-bug.png';
 };
 
-Obstacle.prototype.update = function(dt) {
-
+Obstacle.prototype.update = function(direction) {//need to update the location
+	switch(direction) {
+		case 'left':
+			this.y--;
+		break;
+		case 'right':
+			this.y++;
+		break;
+		default:
+			console.log('Enter a valid direction');
+		break;
+	};
 };
 
 // document.addEventListener('keyup', function(e) {
